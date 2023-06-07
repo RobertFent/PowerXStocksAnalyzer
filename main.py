10d86ed75d947a332985b72e24e0e13172b0e377
RobertFent <info@robertfent.dev>
2023-06-07 15:41:24 +0200

    refactored code; updated rsi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-"""stock analyzer based on PowerXStrategy
-"""
+'''stock analyzer based on PowerXStrategy
+'''
 import concurrent.futures
 import ftplib
 import threading
@@ -7,6 +7,7 @@ import io
 from datetime import datetime, timedelta
 import pytz
 import requests
+import pandas_ta as ta
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import pandas as pd
@@ -25,15 +26,34 @@ MIN_RSI = 50
 MIN_STOCH_K = 50
 # url of API
 BASE_URL = 'https://query1.finance.yahoo.com/v8/finance/chart/'
-
-
-winning_stocks = []
+# header for request to not get forbidden error
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36' +
+           ' (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 LOCK = threading.Lock()
+
+# global dynamic params
+current_date = None
+past_date = None
+params = None
+winning_stocks = []
+
+def init_request_params():
+    '''inits global params used for sending a request.
+    '''
+    global current_date, past_date, params
+    current_date, past_date = get_dates()
+
+    params = {
+        'period1': int(pd.Timestamp(past_date).timestamp()),
+        'period2': int(pd.Timestamp(current_date).timestamp()),
+        'interval': '1d',
+        # 'events': 'div,splits'
+    }
 
 
 def get_sp500_symbols():
-    """returns list of s&p 500 listed symbols.
-    """
+    '''returns list of s&p 500 listed symbols.
+    '''
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     response = requests.get(url, timeout=5)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -48,8 +68,8 @@ def get_sp500_symbols():
 
 
 def get_nasdaq_symbols():
-    """returns list of nasdaq listed symbols.
-    """
+    '''returns list of nasdaq listed symbols.
+    '''
     ftp = ftplib.FTP('ftp.nasdaqtrader.com')
     ftp.login()
     ftp.cwd('SymbolDirectory')
@@ -71,8 +91,8 @@ def get_nasdaq_symbols():
 
 
 def get_dow_jones_symbols():
-    """returns list of dow jones listed symbols.
-    """
+    '''returns list of dow jones listed symbols.
+    '''
     url = 'https://www.investing.com/indices/us-30-components'
     response = requests.get(url, timeout=5)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -93,54 +113,77 @@ def get_dow_jones_symbols():
 
 
 def get_ticker_data(ticker):
-    """returns dataframe with all needed data of given symbol.
+    '''returns dataframe with all needed data of given symbol.
 
     Keyword arguments:
     ticker -- symbol of stock
-    """
-    current_date, past_date = get_dates()
+    '''
     url = BASE_URL + ticker
-    params = {
-        'period1': int(pd.Timestamp(past_date).timestamp()),
-        'period2': int(pd.Timestamp(current_date).timestamp()),
-        'interval': '1d',
-        # 'events': 'div,splits'
-    }
 
     # send request
-    response = requests.get(url, params,
-                            headers={'User-Agent': 'Mozilla/5.0 (Macintosh; ' +
-                                     'Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, ' +
-                                     'like Gecko) Chrome/39.0.2171.95 Safari/537.36'},
-                            timeout=5)
+    response = requests.get(url, params, headers=HEADERS, timeout=5)
 
-    # get JSON response
-    data = response.json()
+    if response.status_code == 200:
+        # get JSON response
+        data = response.json()
 
-    # get open / high / low / close data
-    frame = pd.DataFrame(data['chart']['result'][0]['indicators']['quote'][0])
+        # get open / high / low / close data
+        frame = pd.DataFrame(data['chart']['result']
+                             [0]['indicators']['quote'][0])
 
-    # get the date info
-    temp_time = data['chart']['result'][0]['timestamp']
+        # get the date info
+        temp_time = data['chart']['result'][0]['timestamp']
 
-    # add time
-    frame.index = pd.to_datetime(temp_time, unit='s')
-    frame.index = frame.index.map(lambda dt: dt.floor('d'))
+        # add time
+        frame.index = pd.to_datetime(temp_time, unit='s')
+        frame.index = frame.index.map(lambda dt: dt.floor('d'))
 
-    # reorder frame
-    frame = frame[['open', 'high', 'low', 'close', 'volume']]
-    frame['ticker'] = ticker.upper()
+        # reorder frame
+        frame = frame[['open', 'high', 'low', 'close', 'volume']]
+        frame['ticker'] = ticker.upper()
 
-    return frame
+        return frame
+
+    return None
+
+
+def get_option_strike_price(ticker, target_price):
+    '''todo: exp. etc.
+    '''
+    url = 'https://query2.finance.yahoo.com/v7/finance/options/' + ticker
+    response = requests.get(url, params, headers=HEADERS, timeout=5)
+    if response.status_code == 200:
+        data = response.json()
+        options = data['optionChain']['result'][0]['options'][0]['calls']
+        # iterate over inverted list to start with max call and break at first call lower
+        for option in options[::-1]:
+            strike_price = option['strike']
+            """
+            expiration_date = option['expiration']
+            # contract should expire within 30-45 days
+            future1 = (datetime.now() + timedelta(days=10)).timestamp()
+            future2 = (datetime.now() + timedelta(days=45)).timestamp()
+            if (strike_price < target_price and
+                expiration_date > future1 and
+                    expiration_date < future2):
+                print(option)
+                return option['strike'], option['contractSymbol']
+            """
+            if strike_price < target_price:
+                return strike_price
+
+    else:
+        print('Error occurred:', response.status_code)
+        return None
 
 
 # MACD
 def add_macd_data(dataframe):
-    """adds macd(26, 12, 9) values.
+    '''adds macd(26, 12, 9) values.
 
     Keyword arguments:
     dataframe -- ticker data as pd dataframe
-    """
+    '''
     ema_12 = dataframe['close'].ewm(span=12, adjust=False).mean()
     ema_26 = dataframe['close'].ewm(span=26, adjust=False).mean()
 
@@ -151,14 +194,13 @@ def add_macd_data(dataframe):
     dataframe['Signal Line'] = signal_line
 
 # RSI
-
-
 def add_rsi_data(dataframe):
-    """adds rsi(7) values.
+    '''adds rsi(7) values.
 
     Keyword arguments:
     dataframe -- ticker data as pd dataframe
-    """
+    '''
+    '''old code
     price_changes = dataframe['close'].diff()
 
     # Separate gains and losses
@@ -176,16 +218,19 @@ def add_rsi_data(dataframe):
     rsi = 100 - (100 / (1 + rs))
 
     dataframe['RSI'] = rsi
+    '''
+    rsi = ta.rsi(dataframe['close'], length=7)
+    dataframe['RSI'] = rsi
 
 
 # todo: not always the most accurate result for %K
 # stochastic slow
 def add_stochastic_slow(dataframe):
-    """adds stochastic slow(14, 3, 3) values.
+    '''adds stochastic slow(14, 3, 3) values.
 
     Keyword arguments:
     dataframe -- ticker data as pd dataframe
-    """
+    '''
     # Calculate the lowest low over the past 14 days
     lowest_low = dataframe['low'].rolling(window=14).min()
 
@@ -203,12 +248,11 @@ def add_stochastic_slow(dataframe):
 
 
 def get_dates(time_format='%m/%d/%Y'):
-    """returns dates in est time zone.
+    '''returns dates in est time zone.
 
     Keyword arguments:
     time_format -- wanted format of time
-    """
-    current_date = datetime.now()
+    '''
     est_timezone = pytz.timezone('US/Eastern')
 
     # convert to est
@@ -221,11 +265,11 @@ def get_dates(time_format='%m/%d/%Y'):
 
 
 def add_color_of_days(dataframe):
-    """colors day to green, black or red.
+    '''colors day to green, black or red.
 
     Keyword arguments:
     dataframe -- ticker data as pd dataframe
-    """
+    '''
 
     colors = []
     for i in range(len(dataframe)):
@@ -246,17 +290,18 @@ def add_color_of_days(dataframe):
             colors.append('green')
         elif (green_indicators < 3 and green_indicators > 0):
             colors.append('black')
-        else: colors.append('red')
+        else:
+            colors.append('red')
 
     dataframe['color'] = colors
 
 
 def is_winner(dataframe):
-    """decide if stock is a winner.
+    '''decide if stock is a winner.
 
     Keyword arguments:
     dataframe -- ticker data as pd dataframe
-    """
+    '''
     # current day should be green and the day before not
     return (dataframe.iloc[[len(dataframe)-1]]['color'].item() == 'green' and
             dataframe.iloc[[len(dataframe)-2]]['color'].item() != 'green' and
@@ -266,20 +311,24 @@ def is_winner(dataframe):
 
 
 def add_order_values(dataframe):
-    """add stock and option trading data.
+    '''add stock and option trading data.
 
     Keyword arguments:
     dataframe -- ticker data as pd dataframe
-    """
+    '''
 
-    adr = [None, None, None, None, None, None]
-    entry = [None, None, None, None, None, None]
-    stop_loss = [None, None, None, None, None, None]
-    limit_order = [None, None, None, None, None, None]
-    shares_to_buy = [None, None, None, None, None, None]
+    adr = [None] * 6
+    entry = []
+    stop_loss = [None] * 6
+    limit_order = [None] * 6
+    shares_to_buy = [None] * 6
+    strike_price = [None] * (len(dataframe) - 1)
 
     for i in range(len(dataframe)):
         # print(dataframe.iloc[[i]])
+        # entry for next day
+        next_entry = dataframe.iloc[[i]]['high'].item() + 0.01
+        entry.append(next_entry)
         if i > 5:
             sum_values = 0
             for j in range(i-7, i):
@@ -288,35 +337,36 @@ def add_order_values(dataframe):
 
             # adr of current day
             current_adr = sum_values / 7
-            # entry for next day
-            next_entry = dataframe.iloc[[i]]['high'].item() + 0.01
             # stop_loss and limit order
             stop_loss_current_day = next_entry - current_adr
             limit_order_current_day = next_entry + 2 * current_adr
             # max amount of shares to buy to not loose more than 2% of depot in case of stop-loss
             max_shares_current_day = (
                 DEPOSIT_VALUE*0.02) / (next_entry - stop_loss_current_day)
+
+            # append all needed values
             shares_to_buy.append(max_shares_current_day)
             adr.append(current_adr)
-            entry.append(next_entry)
             stop_loss.append(stop_loss_current_day)
             limit_order.append(limit_order_current_day)
 
-            # todo: option prices
+    strike_price.append(get_option_strike_price(
+        dataframe.iloc[[len(dataframe)-1]]['ticker'].item(), entry[len(entry)-1]))
 
     # dataframe['ADR'] = adr
     dataframe['Next-Entry'] = entry
     dataframe['Stop-Loss'] = stop_loss
     dataframe['Limit-Order'] = limit_order
     dataframe['Max-Shares'] = shares_to_buy
+    dataframe['Strike-Price'] = strike_price
 
 
 def analyze_ticker(ticker):
-    """analyze add all indicators etc. to stock data.
+    '''analyze add all indicators etc. to stock data.
 
     Keyword arguments:
     tickers -- list of all symbols
-    """
+    '''
     try:
         # get data starting past month and ending current date
         ticker_data = get_ticker_data(ticker)
@@ -335,22 +385,22 @@ def analyze_ticker(ticker):
 
 
 def analyze_ticker_wrapper(ticker):
-    """analyze stock and return if winner.
+    '''analyze stock and return if winner.
 
     Keyword arguments:
     tickers -- list of all symbols
-    """
+    '''
     ticker_data = analyze_ticker(ticker)
     if ticker_data is not None and is_winner(ticker_data):
         return ticker_data.iloc[[len(ticker_data)-1]]['ticker'].item()
 
 
 def analyze_stocks(tickers):
-    """analyze all stocks in concurrent threads.
+    '''analyze all stocks in concurrent threads.
 
     Keyword arguments:
     tickers -- list of all symbols
-    """
+    '''
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = list(
             tqdm(executor.map(analyze_ticker_wrapper, tickers), total=len(tickers)))
@@ -360,8 +410,8 @@ def analyze_stocks(tickers):
 
 
 def set_winners():
-    """set all winners of each index.
-    """
+    '''set all winners of each index.
+    '''
     print('Analyzing NASDAQ...')
     tickers = get_nasdaq_symbols()
     # print(tickers)
@@ -375,15 +425,59 @@ def set_winners():
     print('%d stocks in S&P500 analyzed' % len(tickers))
 
 
+def test_data(ticker):
+    """method for testing various params or calcs.
+    """
+    ticker_data = get_ticker_data(ticker)
+
+    rsi = ta.rsi(ticker_data['close'], length=7)
+    ticker_data['rsi_new'] = rsi
+
+    price_changes = ticker_data['close'].diff()
+
+    # Separate gains and losses
+    gains = price_changes.where(price_changes > 0, 0)
+    losses = -price_changes.where(price_changes < 0, 0)
+
+    # Calculate average gain and average loss over 7-day period
+    avg_gain = gains.rolling(window=7, min_periods=1).mean()
+    avg_loss = losses.rolling(window=7, min_periods=1).mean()
+
+    # Calculate relative strength (RS)
+    rs = avg_gain / avg_loss
+
+    # Calculate RSI
+    rsi1 = 100 - (100 / (1 + rs))
+
+    ticker_data['rsi_old'] = rsi1
+
+    ticker_data['%K_new'] = ta.sma(rsi, length=3)
+
+    lowest_low = ticker_data['low'].rolling(window=14).min()
+    highest_high = ticker_data['high'].rolling(window=14).max()
+    percent_k = ((ticker_data['close'] - lowest_low) /
+                 (highest_high - lowest_low)) * 100
+    percent_d = percent_k.rolling(window=3, min_periods=0).mean()
+
+    ticker_data['%K_old'] = percent_k
+    ticker_data['%D_old'] = percent_d
+
+    ticker_data = ticker_data[[
+        'ticker', 'high', 'close', 'rsi_new', 'rsi_old', '%K_new', '%K_old']]
+
+    print(ticker_data)
+
+
 def get_info(ticker, options=False):
-    """prints the ticker data.
+    '''prints the ticker data.
 
     Keyword arguments:
     ticker -- symbol of ticker (e.g. AAPL)
     options -- wether ticker data should be for stocks or option trading
-    """
+    '''
     try:
         ticker_data = get_ticker_data(ticker)
+
         add_order_values(ticker_data)
 
         # debug
@@ -398,11 +492,12 @@ def get_info(ticker, options=False):
         if options:
             # todo: get data for options
             ticker_data = ticker_data[[
-                'ticker', 'high', 'Next-Entry', 'Stop-Loss', 'Limit-Order', 'Max-Shares', 'color']]
+                'ticker', 'high', 'close', 'Next-Entry', 'Strike-Price', 'color']]
             print('Details for OPTIONS-Trading:')
         else:
             ticker_data = ticker_data[[
-                'ticker', 'high', 'Next-Entry', 'Stop-Loss', 'Limit-Order', 'Max-Shares', 'color']]
+                'ticker', 'high', 'close', 'Next-Entry', 'Stop-Loss',
+                'Limit-Order', 'Max-Shares', 'color']]
             print('Details for Stock-Trading:')
 
         print(ticker_data)
@@ -411,9 +506,11 @@ def get_info(ticker, options=False):
     except Exception as e:
         print(str(e))
 
+
 def main():
-    """starts program based on user input.
-    """
+    '''starts program based on user input.
+    '''
+    init_request_params()
     choice = input(
         'Welcome to PowerXStocksAnalyzer!\nWhat do you want to do? 1: get a list of stocks in ' +
         'buy zone or {ticker symbol}: get specific info of a given symbol?: ')
@@ -425,11 +522,13 @@ def main():
             print('https://finance.yahoo.com/chart/' + winner)
     else:
         choice1 = input(
-            'Do you trade stocks or options? 1=stocks; 2=options: ')
+            'Do you trade stocks or options? 1=stocks; 2=options; 3=debugging: ')
         if choice1 == '1':
             get_info(choice)
         elif choice1 == '2':
             get_info(choice, True)
+        elif choice1 == '3':
+            test_data(choice)
         else:
             print('Wrong input!')
 
